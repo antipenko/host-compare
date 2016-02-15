fdc5ae999200145f51541244add3994d8fafa9d8
antipenko <sweetglory.victory@gmail.com>
2016-02-15 12:05:47 +0200

    first

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-$(document).foundation();
+;(function($){
+	$(function(){
+		var slider=$('.ba-slider');
+
+		$('.ba-featured-slider').slick({
+			dots: true,
+   			infinite:false,
+			arrow:false,
+			prevArrow: false,
+			nextArrow:false
+
+		});
+		console.log('good');
+	});
+})(jQuery);
