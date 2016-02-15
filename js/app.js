4860e7d5472e3578a6a01d555341e1a69256f8f3
antipenko <sweetglory.victory@gmail.com>
2016-02-15 18:09:01 +0200

    Some descriptive commit message

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 ;(function($){
 	$(function(){
+		var slider=$('.ba-baner-slider');
+
+		slider.slick({
+			// dots: true,
+   // 			infinite:false
+
+		});
+		console.log('good');
+	});
+
+
+	$(function(){
 		var slider=$('.ba-slider');
 
 		$('.ba-featured-slider').slick({
